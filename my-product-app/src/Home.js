7f285678073b04a6d56b0d9bb31772282cc47601
SettawutInthong <golfy26@hotmail.co.th>
2023-03-25 01:18:53 +0700

    เหลือตกแต่งนิดหน่อย

--- my-product-app/src/Home.js
+++ my-product-app/src/Home.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import { API_GET, API_POST } from "./api";
 import ProductItem from "./ProductItem";
+import "./Home.css";
+import img1 from './images/istockphoto-1254474165-170667a.jpg'
 
 export default function Home() {
 
@@ -69,37 +71,51 @@ export default function Home() {
 
     if (localStorage.getItem("access_token")) {
         return (
-            <div className="container">
-                <select value={productTypeId} onChange={(e) => setProductTypeId(e.target.value)}>
-                    <option value={0}>ทุกประเภทสินค้า</option>
-                    {
-                        productTypes.map(item => (
+            <div className="background">
+                <div className="header">
+                    <a href="#" className="logo">
+                        Green plantnet
+                    </a>
+                </div>
+                <img src={img1} alt="Flower" className="flower-image" />
+                <div className="search-container">
+                    <input type="text" className="search-input" placeholder="Search..." />
+                </div>
+                <div className="categories">
+                    <select
+                        value={productTypeId}
+                        onChange={(e) => setProductTypeId(e.target.value)}
+                        className="categories"
+                    >
+                        <option value={0}>ทุกประเภทสินค้า</option>
+                        {productTypes.map((item) => (
                             <option key={item.product_type_id} value={item.product_type_id}>
                                 {item.product_type}
                             </option>
-                        ))
-                    }
-                </select>
+                        ))}
+                    </select>
 
-                <Link to={"/product/add"} className="btn btn-outline-primary me-3">เพิ่ม</Link>
+                    <div className="buttons-container">
+                        <Link to={"/product/add"} className="btn btn-outline-primary me-3">
+                            เพิ่ม
+                        </Link>
 
-                <div className="container mt-3">
-                    {
-                        products.map(item => (
-                            <ProductItem 
-                            key={item.product_id}
-                            data={item} 
-                            onDelete={onDelete}/>
-                        ))
-                    }
+                        <Link to={"/report"} className="btn btn-outline-primary me-3">
+                            รายงาน
+                        </Link>
+                    </div>
+
+                    <div className="categories-right"></div>
+
+                    <div className="container mt-3">
+                        {products.map((item) => (
+                            <ProductItem key={item.product_id} data={item} onDelete={onDelete} />
+                        ))}
+                    </div>
                 </div>
             </div>
-
         );
-
     }
 
-    return (
-        <Navigate to="/" replace />
-    );
-}
+    return (<Navigate to="/" replace />);
+} 
